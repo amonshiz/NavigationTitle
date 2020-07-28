848331026eca6a5b48aaeb5484c150c4e813f51a
Andrew Monshizadeh <amonshiz@gmail.com>
2020-07-27 22:26:43 -0500

    Correct access levels and properly annotate availability
    - Added ability to find correct VC from a navigation button
    - Removed extension on `View` because no longer makes sense, should
      always be a toolbar button

--- Sources/NavigationTitleView/NavigationTitleView.swift
+++ Sources/NavigationTitleView/NavigationTitleView.swift
@@ -3,27 +3,15 @@ import SwiftUI
 #if os(iOS) || os(tvOS)
 import UIKit
 
-private class NavigationTitleViewImpl<Content>: UIView where Content: View{
+public final class NavigationTitleViewImpl<Content>: UIView where Content: View{
   fileprivate var titleViewContent: UIHostingController<Content>?
   fileprivate var vcForNavigationItem: UIViewController?
 
-  override func didMoveToWindow() {
-    if window != nil {
+  public override func didMoveToWindow() {
+    if window != nil,
+       vcForNavigationItem == nil {
       vcForNavigationItem = getNavItemVC()
-    } else {
-      if let nivc = vcForNavigationItem,
-         let tv = nivc.navigationItem.titleView,
-         let tvc = titleViewContent,
-         tv == tvc.view {
-        nivc.navigationItem.titleView = nil
-        vcForNavigationItem = nil
-        titleViewContent = nil
-      }
     }
-  }
-
-  deinit {
-    print("Deinit")
   }
 
   private func getNavItemVC() -> UIViewController? {
@@ -33,6 +21,10 @@ private class NavigationTitleViewImpl<Content>: UIView where Content: View{
       if let vc = nextResponder as? UIViewController,
          vc.navigationController != nil {
         return vc
+      } else if let nb = nextResponder as? UINavigationBar,
+                let nbdelegate = nb.delegate as? UINavigationController,
+                let topVC = nbdelegate.topViewController {
+        return topVC
       }
 
       nextResponder = nextResponder?.next
@@ -42,33 +34,36 @@ private class NavigationTitleViewImpl<Content>: UIView where Content: View{
   }
 }
 
-private struct FixedSizeView<Content>: View where Content: View {
+public struct FixedSizeView<Content>: View where Content: View {
   let content: Content
-  var body: some View {
+  public var body: some View {
     content.fixedSize()
   }
 }
 
-private struct NavigationTitleView<Content>: UIViewRepresentable where Content: View {
-  typealias InnerContent = FixedSizeView<Content>
+@available(iOS 13.0, tvOS 13.0, *)
+@available(macOS, unavailable)
+@available(watchOS, unavailable)
+public struct NavigationTitleView<Content>: UIViewRepresentable where Content: View {
+  public typealias InnerContent = FixedSizeView<Content>
 
   let content: Content
 
-  init(content: Content) {
+  public init(content: Content) {
     self.content = content
   }
 
-  func makeUIView(context: Context) -> NavigationTitleViewImpl<InnerContent> {
-    NavigationTitleViewImpl<FixedSizeView<Content>>()
+  public func makeUIView(context: Context) -> NavigationTitleViewImpl<InnerContent>  {
+    NavigationTitleViewImpl<InnerContent>()
   }
 
-  func updateUIView(_ uiView: NavigationTitleViewImpl<InnerContent>, context: Context) {
+  public func updateUIView(_ uiView: NavigationTitleViewImpl<InnerContent>, context: Context) {
     // only need to set stuff up if the view has found a parent view controller
     // with a navigation controller
     if let vc = uiView.vcForNavigationItem {
       // when SwiftUI updates the view structs the struct within the
       // UIHostingController will be out of date and not automatically updated,
-      // thus it has to be manually replaced. the `TitleViewImpl` is simpley a
+      // thus it has to be manually replaced. the `TitleViewImpl` is simply a
       // place to store references and does not actually instantiate the
       // hosting controller so that there is only 1 canonical location to do so.
       if let tvc = uiView.titleViewContent {
@@ -81,23 +76,3 @@ private struct NavigationTitleView<Content>: UIViewRepresentable where Content: 
   }
 }
 #endif
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-public extension View {
-  func navigationTitleView<Content>(@ViewBuilder _ content: () -> Content) -> some View where Content: View {
-    #if os(iOS) || os(tvOS)
-    return self.overlay(
-      NavigationTitleView(content: content())
-        // prevent SwiftUI from directing all touches to the `TitleView`
-        // instance's underlying view, even if the `UIView` object that is created
-        // because it is a `UIViewRepresentable` does not accept any touches.
-        // can verify this by adding empty implementations of the methods
-        // `point(inside:with:)` and `hitTest(_:with:)` to `TitleViewImpl`
-        .allowsHitTesting(false)
-    )
-    #else
-    return content()
-    #endif
-  }
-}
-
--- Tests/NavigationTitleViewTests/NavigationTitleViewTests.swift
+++ Tests/NavigationTitleViewTests/NavigationTitleViewTests.swift
@@ -2,15 +2,24 @@ import XCTest
 import SwiftUI
 @testable import NavigationTitleView
 
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 final class NavigationTitleViewTests: XCTestCase {
+  private func addToolbar<Content>(_ content: Content) -> some View where Content: View {
+    #if os(iOS) || os(tvOS)
+      return content
+      .toolbar {
+        ToolbarItem {
+          NavigationTitleView(content: Text("Else"))
+        }
+      }
+    #else
+    return content
+    #endif
+  }
+
   func testExample() {
     let content =
-      HStack {
-        Text("Something")
-      }
-      .navigationTitleView{
-        Text("Else")
-      }
+      addToolbar(HStack { Text("Something") })
     let contentMirror = Mirror(reflecting: content)
 
     #if os(iOS) || os(tvOS)
