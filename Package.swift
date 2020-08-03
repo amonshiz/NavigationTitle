23cae37c9a6b0cb1df38da949854408d6ac168fa
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-03 17:03:10 -0500

    Rename to NavigationTitle

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "NavigationTitleView",
+  name: "NavigationTitle",
   platforms: [
     .iOS(.v13), .tvOS(.v13)
   ],
   products: [
     .library(
-      name: "NavigationTitleView",
-      targets: ["NavigationTitleView"]),
+      name: "NavigationTitle",
+      targets: ["NavigationTitle"]),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "NavigationTitleView",
+      name: "NavigationTitle",
       dependencies: []),
     .testTarget(
-      name: "NavigationTitleViewTests",
-      dependencies: ["NavigationTitleView"])
+      name: "NavigationTitleTests",
+      dependencies: ["NavigationTitle"])
   ]
 )
--- Tests/NavigationTitleTests/NavigationTitleTests.swift
+++ Tests/NavigationTitleTests/NavigationTitleTests.swift
@@ -1,6 +1,6 @@
 import XCTest
 import SwiftUI
-@testable import NavigationTitleView
+@testable import NavigationTitle
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 final class NavigationTitleTests: XCTestCase {
